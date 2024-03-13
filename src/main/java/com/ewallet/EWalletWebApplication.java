69bddce11fdf365a91831869c5e7f06623b14817
juchengb <fjchengou@gmail.com>
2024-03-13 13:06:30 +0800

    update

---
+++ src/main/java/com/ewallet/EWalletWebApplication.java
@@ -0,0 +1,13 @@
+package com.ewallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EWalletWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EWalletWebApplication.class, args);
+	}
+
+}
